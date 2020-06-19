169cfa23540eef9e512f4089181000875f149655
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-06-19 15:38:57 +0530

    added side bar

---
+++ 2. SideBar/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+  });
+});
